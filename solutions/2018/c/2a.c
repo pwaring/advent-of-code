4781799bf627c0e90d60ce8c42824e3947c88059
Paul Waring <paul@xk7.net>
2018-12-02 16:39:27 +0000

    Solution to 2018 2a

--- solutions/2018/c/2a.c
+++ solutions/2018/c/2a.c
@@ -7,6 +7,7 @@
 #include <glib.h>
 
 #include "functions.h"
+#include "macros.h"
 
 int main(void)
 {
@@ -16,45 +17,81 @@ int main(void)
   const size_t maximum_line_length = 50;
   char *current_line = calloc(maximum_line_length, sizeof(char));
 
+  char *letters = calloc(maximum_line_length, sizeof(char));
+  uint8_t *frequencies = calloc(maximum_line_length, sizeof(uint8_t));
+
   while ((current_line = fgets(current_line, maximum_line_length, stdin)) != NULL)
   {
     // Remove trailing line feeds and carriage returns
     current_line[strcspn(current_line, "\r\n")] = '\0';
 
-    GTree *letter_frequencies = g_tree_new(compare_char);
-    gpointer old_letter_count = NULL;
-    uint8_t new_letter_count = 0;
-
-    // Build the tree of letter frequencies
-    for (uint8_t i = 0; current_line[i] != '\0'; i++)
+    // Clear the letters and frequencies arrays
+    for (uint8_t i = 0; i < maximum_line_length; i++)
     {
-      old_letter_count = g_tree_lookup(letter_frequencies, GCHAR_TO_POINTER(current_line[i]));
+      letters[i] = '\0';
+      frequencies[i] = 0;
+    }
 
-      if (old_letter_count == NULL)
+    // Iterate over each character in the current line
+    for (uint8_t c = 0; current_line[c] != '\0'; c++)
+    {
+      char current_char = current_line[c];
+      bool char_found = false;
+
+      // Check if letter is already in array
+      for (uint8_t i = 0; i < maximum_line_length && !char_found; i++)
       {
-        // Letter has not been seen before, so add it to the tree
-        new_letter_count = 1;
-        g_tree_insert(letter_frequencies, GCHAR_TO_POINTER(current_line[i]), GUINT_TO_POINTER(new_letter_count));
-      }
-      else
-      {
-        // Letter has been seen before, so increment count by one
-        new_letter_count = GPOINTER_TO_UINT(old_letter_count);
-        new_letter_count++;
-        g_tree_replace(letter_frequencies, GCHAR_TO_POINTER(current_line[i]), GUINT_TO_POINTER(new_letter_count));
+        // If letter is in array, increment by 1
+        if (letters[i] == current_char)
+        {
+          char_found = true;
+          frequencies[i] = frequencies[i] + 1;
+        }
+
+        // If letter is the NUL character, we have reached the end of the list,
+        // so insert this letter with a frequency of 1
+        if (letters[i] == '\0')
+        {
+          char_found = true;
+          letters[i] = current_char;
+          frequencies[i] = 1;
+        }
       }
     }
 
-    // Traverse the tree to find 2 and 3 letter frequencies - but only count them once
-    bool two_letter_found = false;
-    bool three_letter_found = false;
+    // We now have the frequencies for each character, so check if any have a
+    // frequency of 2 or 3. We only want to match once for each frequency, so
+    // if there are 2+ letters with a frequency of two that only counts once.
+    bool two_found = false;
+    bool three_found = false;
 
-    
-
-    g_tree_destroy(letter_frequencies);
-    letter_frequencies = NULL;
+    for (uint8_t i = 0; i < maximum_line_length && (!two_found || !three_found); i++)
+    {
+      if (frequencies[i] == 2 && !two_found)
+      {
+        two_letter_count++;
+        two_found = true;
+      }
+      else if (frequencies[i] == 3 && !three_found)
+      {
+        three_letter_count++;
+        three_found = true;
+      }
+    }
   }
+
+  free(frequencies);
+  frequencies = NULL;
+
+  free(letters);
+  letters = NULL;
 
   free(current_line);
   current_line = NULL;
+
+  uint32_t checksum = two_letter_count * three_letter_count;
+
+  printf("Checksum: %" PRIu32 "\n", checksum);
+
+  return EXIT_SUCCESS;
 }
--- solutions/2018/c/functions.h
+++ solutions/2018/c/functions.h
@@ -16,22 +16,3 @@ gint compare_int(gconstpointer a, gconstpointer b)
     return 1;
   }
 }
-
-gint compare_char(gconstpointer a, gconstpointer b)
-{
-  char a_comp = GPOINTER_TO_CHAR(a);
-  char b_comp = GPOINTER_TO_CHAR(b);
-
-  if (a_comp < b_comp)
-  {
-    return -1;
-  }
-  else if (a_comp == b_comp)
-  {
-    return 0;
-  }
-  else
-  {
-    return 1;
-  }
-}
