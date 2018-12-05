16ca2ae131c87f6537943bbe2f6d578c40dbab8c
Paul Waring <paul@xk7.net>
2018-12-05 20:28:59 +0000

    Indirect header for glib whilst disabling -Weverything

---
+++ solutions/2018/c/glib_indirect.h
@@ -0,0 +1,10 @@
+#ifdef __clang__
+#  pragma clang diagnostic push
+#  pragma clang diagnostic ignored "-Weverything"
+#endif
+
+#include <glib.h>
+
+#ifdef __clang__
+#  pragma clang diagnostic pop
+#endif
