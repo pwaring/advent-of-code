7e75e5832e62431b8012c05ed1334b87000fd064
Paul Waring <paul@xk7.net>
2020-11-27 21:18:24 +0000

    2015 Day 4 Part A solution
    Update to make leading zero count configurable.

--- 2015/04/4a.php
+++ 2015/04/4a.php
@@ -6,12 +6,15 @@ error_reporting(E_ALL);
 $secret_key = trim(file_get_contents('input'));
 $number = 0;
 
+$leading_zero_count = 5;
+$leading_zero_str = str_repeat('0', $leading_zero_count);
+
 while (true)
 {
     $str = $secret_key . strval($number);
     $hash = md5($str);
 
-    if (substr($hash, 0, 5) === '00000')
+    if (substr($hash, 0, $leading_zero_count) === $leading_zero_str)
     {
         break;
     }
