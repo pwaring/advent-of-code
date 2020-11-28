ecd182ae170e1f7984353584236d78cb73aa51a6
Paul Waring <paul@xk7.net>
2020-11-28 17:19:32 +0000

    2015 Day 4 Part B solution

---
+++ 2015/04/4b.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+error_reporting(E_ALL);
+
+$secret_key = trim(file_get_contents('input'));
+$number = 0;
+
+$leading_zero_count = 6;
+$leading_zero_str = str_repeat('0', $leading_zero_count);
+
+while (true)
+{
+    $str = $secret_key . strval($number);
+    $hash = md5($str);
+
+    if (substr($hash, 0, $leading_zero_count) === $leading_zero_str)
+    {
+        break;
+    }
+
+    $number++;
+}
+
+print("Lowest number to mine AdventCoin: $number\n");
