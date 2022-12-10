eeaa8e993bcf17dc3099f7cf846a5fcfe939d4d2
Paul Waring <paul@xk7.net>
2022-12-10 14:24:38 +0000

    2022 Day 6 Part B: Go

---
+++ 2022/06/6b.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	const chunkLength = 14
+
+	// ASSUMPTION: There is only one line of input and no errors
+	byteInput, _ := os.ReadFile(os.Stdin.Name())
+	input := string(byteInput)
+
+	for start, end := 0, chunkLength-1; end < len(input); start, end = start+1, end+1 {
+		// Add one to end because second array slice index is exclusive
+		chunk := input[start : end+1]
+		characters := strings.Split(chunk, "")
+		seen := make(map[string]bool)
+
+		for c := range characters {
+			seen[characters[c]] = true
+		}
+
+		if len(seen) == chunkLength {
+			// All characters are unique
+			fmt.Println(end + 1)
+			os.Exit(0)
+		}
+	}
+}
